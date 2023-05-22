5f04670f8aaf8daa06ccb44134cedeef1e03359a
OITCORUMER <OITCORUMER@VACOLTRUMER.dva.va.gov>
2023-05-22 14:02:05 -0400

    Clean Up
    Clean up pom file
    Clean up page class

--- vapars-automation/src/test/java/gov/va/cpac/VAPARS/Pages/CreateAVeteranPage.java
+++ vapars-automation/src/test/java/gov/va/cpac/VAPARS/Pages/CreateAVeteranPage.java
@@ -1,94 +1,52 @@
 package gov.va.cpac.VAPARS.Pages;
 
-import java.time.Duration;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import gov.va.him.webapp.BasePage;
 
-public class CreateAVeteranPage {
-	WebDriver driver;
-
-	public CreateAVeteranPage(WebDriver driver) {
-		this.driver = driver;
-	}
+public class CreateAVeteranPage extends BasePage {
 	
-	By tabVeteran = By.xpath("/html/body/div[1]/div[1]/div/nav/div[3]/ul/li[4]/a");
-	public void clickTabVeteran() {
-		driver.findElement(tabVeteran).click();
-	}
+	@FindBy(xpath = "/html/body/div[1]/div[1]/div/nav/div[3]/ul/li[4]/a")
+	public WebElement tabVeteran;
 	
-	By buttonNewVeteran = By.xpath("//div[@id='main-wrapper']/div[8]/div/div/div[1]/button");
-	public void clickButtonNewVeteran() {
-		driver.findElement(buttonNewVeteran).click();	
-	}
-
-	By fieldSSN = By.xpath("/html/body/div[1]/div[1]/div/div[2]/div[8]/div/div/div[3]/div/div[1]/div/form/div[3]/div/div[1]/input");
-	public void inputSSN(String string) {
-		driver.findElement(fieldSSN).sendKeys(string);
-	}
-
-	By fieldICN = By.id("PatientDataModel_IntegrationControlNumber");
-	public void inputICN(String string) {
-		driver.findElement(fieldICN).sendKeys(string);	
-	}
+	@FindBy(xpath = "//div[@id='main-wrapper']/div[8]/div/div/div[1]/button")
+	public WebElement buttonNewVeteran;
 	
-	By fieldLastName = By.id("PatientDataModel_LastName");
-	public void inputLastName(String string) {
-		driver.findElement(fieldLastName).sendKeys(string);
-	}
-
-	By fieldFirstName = By.id("PatientDataModel_FirstName");
-	public void inputFirstName(String string) {
-		driver.findElement(fieldFirstName).sendKeys(string);
-	}
-
-	By buttonSavePatient = By.xpath("//*[@id='savePatientButton']");
-	public void clickSavePatient() throws InterruptedException {
-        for(int i=0; i<5; i++){
-			driver.findElement(By.tagName("html")).sendKeys(Keys.chord(Keys.CONTROL,Keys.SUBTRACT));
-		}
-     	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-    	wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='savePatientButton']")));
-    	driver.findElement(buttonSavePatient).click();    
-    	Thread.sleep(5000);
-    	driver.findElement(By.tagName("html")).sendKeys(Keys.chord(Keys.CONTROL, "0"));
-	}
-
-	By buttonNewAddress = By.id("newAddressButton");
-	public void clickNewAddress() {
-		driver.findElement(buttonNewAddress).click();
-	}
-
-	By fieldAddress = By.id("PatientAddressDataModel_AddressOne");
-	public void inputAddress(String string) {
-		driver.findElement(fieldAddress).sendKeys(string);
-	}
-
-	By fieldPostalCode = By.id("postalCodeSearch");
-	public void inputPostalCode(String string) {
-		driver.findElement(fieldPostalCode).sendKeys(string);
-	}
-
-	By buttonSearch = By.xpath("//*[@id='patientAddressForm']/div[4]/div/div/div/button");
-	public void clickbuttonSearch() {
-		driver.findElement(buttonSearch).click();
-	}
+	@FindBy(xpath = "/html/body/div[1]/div[1]/div/div[2]/div[8]/div/div/div[3]/div/div[1]/div/form/div[3]/div/div[1]/input")
+	public WebElement fieldSSN;
 	
-	By buttonSelectPC = By.xpath("/html/body/div[1]/div[1]/div/div[2]/div[1]/div/div/div/div/div/div[2]/div/div[2]/div/form/div[5]/div/div/table/tbody/tr/td[3]/button");
-	public void clickSelectPC() {
-		driver.findElement(buttonSelectPC).click();
-	}
+	@FindBy(id = "PatientDataModel_IntegrationControlNumber")
+	public WebElement fieldICN;
 
-	By buttonSaveAddress = By.id("patientAddressSubmitButton");
-	public void clickSaveAddress() {
-		driver.findElement(buttonSaveAddress).click();
-	}
+	@FindBy(id = "PatientDataModel_LastName")
+	public WebElement fieldLastName;
+
+	@FindBy(id = "PatientDataModel_FirstName")
+	public WebElement fieldFirstName;
+
+	@FindBy(xpath = "//*[@id='savePatientButton']")
+	public WebElement buttonSavePatient;
+
+	@FindBy(id = "newAddressButton")
+	public WebElement buttonNewAddress;
+
+	@FindBy(id = "PatientAddressDataModel_AddressOne")
+	public WebElement fieldAddress;
+
+	@FindBy(id = "postalCodeSearch")
+	public WebElement fieldPostalCode;
+
+	@FindBy(xpath = "//*[@id='patientAddressForm']/div[4]/div/div/div/button")
+	public WebElement buttonSearch;
 	
-	By buttonCloseAddress = By.xpath("/html/body/div[1]/div[1]/div/div[2]/div[1]/div/div/div/div/div/div[2]/div/div[2]/div/form/div[9]/button");
-	public void clickCloseAddress() {
-		driver.findElement(buttonCloseAddress).click();
-	}
+	@FindBy(xpath = "/html/body/div[1]/div[1]/div/div[2]/div[1]/div/div/div/div/div/div[2]/div/div[2]/div/form/div[5]/div/div/table/tbody/tr/td[3]/button")
+	public WebElement buttonSelectPC;
+
+	@FindBy(id = "patientAddressSubmitButton")
+	public WebElement buttonSaveAddress;
+	
+	@FindBy(xpath = "/html/body/div[1]/div[1]/div/div[2]/div[1]/div/div/div/div/div/div[2]/div/div[2]/div/form/div[9]/button")
+	public WebElement buttonCloseAddress;
+
 	
 }
