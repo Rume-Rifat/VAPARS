bc6e66bd925dce7ed4784d3fc8152aa99c8db3eb
OITCORUMER <OITCORUMER@VACOLTRUMER.dva.va.gov>
2023-05-30 13:01:55 -0400

    Changes
    Changes to pom file
    Changes to Test Runner
    deleted vapars-automation

--- src/test/java/gov/va/cpac/vapars/steps/StepsRunner/TestRunner.java
+++ src/test/java/gov/va/cpac/vapars/steps/StepsRunner/TestRunner.java
@@ -10,7 +10,7 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, json:target/cucumber-report/cucumber.json")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "gov.va.him.test.TestListener, pretty, json:target/cucumber-report/cucumber.json")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "gov.va.him, gov.va.cpac.vapars.steps")
 @ConfigurationParameter(key= EXECUTION_DRY_RUN_PROPERTY_NAME, value="false")
 
